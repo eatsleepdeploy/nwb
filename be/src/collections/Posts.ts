5e176910bd519ac2291c19448e75ac271f8dc8cb
Jason Kelly <jason@jasonkelly.dev>
2026-02-14 16:00:28 +0900

    inital commit

---
+++ be/src/collections/Posts.ts
@@ -0,0 +1,32 @@
+import type { CollectionConfig } from 'payload'
+
+export const Posts: CollectionConfig = {
+  slug: 'posts',
+  admin: {
+    useAsTitle: 'title',
+  },
+  access: {
+    read: () => true,
+  },
+  fields: [
+        {
+          name: 'title',
+          type: 'text',
+        },
+        {
+          name: 'content',
+          type: 'textarea',
+        },
+        {
+          name: 'subcontent',
+          type: 'richText',
+        },
+        // {
+        //   name: 'content',
+        //   type: 'blocks',
+        //   blocks: [
+        //
+        //   ]
+        // },
+  ],
+}
