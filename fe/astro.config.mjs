05f41a81ebcee48f037c39f20ee5ec83dd06d9a1
Jason Kelly <jason@jasonkelly.dev>
2026-03-07 14:25:42 +0900

    Working tailwind FE styling

--- fe/astro.config.mjs
+++ fe/astro.config.mjs
@@ -1,9 +1,13 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import {defineConfig} from 'astro/config';
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-    image: {
-    domains: ['localhost:3000', 'localhost'], // Add the domain(s) of your image API/CDN
+  image: {
+      domains: ['localhost:3000', 'localhost'], // Add the domain(s) of your image API/CDN
+  },
+  vite: {
+    plugins: [tailwindcss()],
   },
 });
